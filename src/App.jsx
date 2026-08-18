82634392a0ae72d053a5ea24b3029c660ea99486
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-08-18 09:20:33 +0530

    Navbar component has been added with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
