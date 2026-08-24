062a30a14a70e315d6892c82aa3a141cfa2ec6c9
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-08-24 09:27:54 +0530

    API BASE for docs & other file code has been added in services api.js

--- src/services/api.js
+++ src/services/api.js
@@ -35,4 +35,7 @@ api.interceptors.response.use(
   },
 );
 
+// -------------- API BASE -----------------------//
+export const API_BASE = import.meta.env.VITE_API_URL.replace("/api", "");
+
 export default api;
